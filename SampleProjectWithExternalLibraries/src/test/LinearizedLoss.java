ba895d75702339cfe944beb077f46c88dc8d45e1
Finx <mail@gmail.com>
2018-04-24 10:32:56 +0200

    Aggiornamento 24.04

--- SampleProjectWithExternalLibraries/src/test/LinearizedLoss.java
+++ SampleProjectWithExternalLibraries/src/test/LinearizedLoss.java
@@ -2,41 +2,53 @@ package test;
 
 import java.io.IOException;
 
+import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.exception.OutOfRangeException;
+
+
+
 public class LinearizedLoss {
 	
 	
-	public static double Mean(Portfolio savings, int delta_t) throws IOException {
+	public static double Mean(Portfolio savings, double delta_t) throws IOException {
 		double sum = 0;
 		for(int i=0; i < savings.numberofAssets; i++) {
-			double ExpectationXasset = savings.getInitialState(savings.History)[i]
-					*(-Math.pow(savings.volatilities(savings.History)[i], 2)/2)*delta_t;
+			double ExpectationXasset = savings.initialState[i]
+					*(-Math.pow(savings.volatilities[i], 2)/2)*delta_t;
 			sum =+ ExpectationXasset;
 			
 		}
 		return -sum;
 		}
 	
-	public static double Variance(Portfolio savings, int delta_t) throws IOException {
+	public static double Variance(Portfolio savings, double delta_t) throws IOException {
 		
 		double sum = 0;
 		if(savings.numberofAssets==1) {
-			double d = savings.volatilities[0]*savings.volatilities[0]*savings.getInitialState(savings.History)[0]
-					*savings.getInitialState(savings.History)[0]*delta_t;
+			double d = savings.volatilities[0]*savings.volatilities[0]*savings.initialState[0]
+					*savings.initialState[0]*delta_t;
 			return d;
 		} else {
 		for(int i=0; i < savings.numberofAssets; i++) {
 			for(int j=0; j< savings.numberofAssets; j++) {
-				double c = savings.getInitialState(savings.History)[i]*savings.getInitialState(savings.History)[j]*
-						savings.logcorrelationMatrix[i][j]*savings.volatilities[i]*savings.volatilities[j]*delta_t;
+				double c = savings.initialState[i]*savings.initialState[j]*
+						savings.logreturncorrelationMatrix[i][j]*savings.volatilities[i]*savings.volatilities[j]*delta_t;
 				sum =+ c;
 			}
-			
 		}
 		return sum;
+		}
+		}
+	
+	public static double getVar(Portfolio savings, double delta_t, double p) throws OutOfRangeException, IOException {
 		
+		NormalDistribution normal = new NormalDistribution();
+		double var = LinearizedLoss.Mean(savings, delta_t)+Math.sqrt(LinearizedLoss.Variance(savings, delta_t))*
+				normal.inverseCumulativeProbability(p);
+		return var;
 		
 	}
-}
+	
 }
 	
 	
